bb5e5281c3f01ac11973bc3f6b55402234ad0da7
Elliott R. Wang <elliott.wang@nasa.gov>
2019-11-07 22:50:32 -0800

    a second update to submodule

--- misc.py
+++ misc.py
@@ -7,3 +7,11 @@ def add_one(x):
     """
 
     return x + 1
+
+def sub_one(x):
+    """Subtracts one from the input integer
+        input: input
+        output: input - 1
+    """
+
+    return x - 1
